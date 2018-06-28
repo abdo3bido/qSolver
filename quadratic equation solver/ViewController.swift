558409655ff91b5eb59934f75a3a61fa8ec61a14
Abd-ElRahman Shalaby <abdo@Abd-ElRahmans-iMac.local>
2018-06-28 13:43:06 +0200

    COMMIT 2 APP WORKING BUT UI IS RUBBISH

--- quadratic equation solver/ViewController.swift
+++ quadratic equation solver/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
 
@@ -20,6 +21,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet var a: UITextField!
+    @IBOutlet var b: UITextField!
+    @IBOutlet var c: UITextField!
+    @IBOutlet var x1: UILabel!
+    @IBOutlet var x2: UILabel!
+    
+    @IBAction func solve(_ sender: UIButton) {
+        let va = Double(a.text!);
+        let vb = Double(b.text!);
+        let vc = Double(c.text!);
+        let discriminant = sqrt((vb!*vb!)-4*va!*vc!)
+        let x_1 = (-vb! + discriminant)/2*va!;
+        let x_2 = (-vb! - discriminant)/2*va!;
+        x1.text = String(x_1);
+        x2.text = String(x_2);
+        
+        
+        
+    }
 
 }
 
